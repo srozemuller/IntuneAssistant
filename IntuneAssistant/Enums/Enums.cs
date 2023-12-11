11cdc16f8df2d950a7c5e6cf695f48e14026be19
srozemuller <s.rozemuller@exite.com>
2023-12-11 09:27:12 +0100

    Refactored assignment services
    Changed Graph SDK models for own assignments models

---
+++ IntuneAssistant/Enums/Enums.cs
@@ -0,0 +1,26 @@
+namespace IntuneAssistant.Enums;
+
+
+    public enum FixedOptions
+    {
+        table,
+        json,
+        csv,
+    }
+
+public enum ResourceTypes
+{
+    UpdateRingConfiguration,
+    CompliancePolicy,
+    ConfigurationPolicy,
+    DeviceHealthScript,
+    DeviceManagementScript,
+    WindowsAutopilotDeploymentProfile,
+    MobileApp,
+    AppConfigurationPolicy,
+    WindowsManagedAppProtection,
+    IosManagedAppProtection,
+    AndroidManagedAppProtection,
+    WindowsFeatureUpdate,
+    WindowsDriverUpdate
+}
