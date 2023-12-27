00d9a91f890763a36af9beb8f1b007618c3a369d
srozemuller <s.rozemuller@exite.com>
2023-12-27 08:41:39 +0100

    Removed unused compliance properties from model

--- IntuneAssistant/Models/CompliancePolicyModel.cs
+++ IntuneAssistant/Models/CompliancePolicyModel.cs
@@ -12,42 +12,6 @@ public class CompliancePolicy
     public string Description { get; set; }
     public DateTime LastModifiedDateTime { get; set; }
     public string DisplayName { get; set; }
-    public int Version { get; set; }
-    public bool PasswordRequired { get; set; }
-    public bool PasswordBlockSimple { get; set; }
-    public bool PasswordRequiredToUnlockFromIdle { get; set; }
-    public int? PasswordMinutesOfInactivityBeforeLock { get; set; }
-    public int? PasswordExpirationDays { get; set; }
-    public int? PasswordMinimumLength { get; set; }
-    public int? PasswordMinimumCharacterSetCount { get; set; }
-    public string PasswordRequiredType { get; set; }
-    public int? PasswordPreviousPasswordBlockCount { get; set; }
-    public bool RequireHealthyDeviceReport { get; set; }
-    public string OsMinimumVersion { get; set; }
-    public string OsMaximumVersion { get; set; }
-    public string MobileOsMinimumVersion { get; set; }
-    public string MobileOsMaximumVersion { get; set; }
-    public bool EarlyLaunchAntiMalwareDriverEnabled { get; set; }
-    public bool BitLockerEnabled { get; set; }
-    public bool SecureBootEnabled { get; set; }
-    public bool CodeIntegrityEnabled { get; set; }
-    public bool MemoryIntegrityEnabled { get; set; }
-    public bool KernelDmaProtectionEnabled { get; set; }
-    public bool VirtualizationBasedSecurityEnabled { get; set; }
-    public bool FirmwareProtectionEnabled { get; set; }
-    public bool StorageRequireEncryption { get; set; }
-    public bool ActiveFirewallRequired { get; set; }
-    public bool DefenderEnabled { get; set; }
-    public string DefenderVersion { get; set; }
-    public bool SignatureOutOfDate { get; set; }
-    public bool RtpEnabled { get; set; }
-    public bool AntivirusRequired { get; set; }
-    public bool AntiSpywareRequired { get; set; }
-    public bool DeviceThreatProtectionEnabled { get; set; }
-    public string DeviceThreatProtectionRequiredSecurityLevel { get; set; }
-    public bool ConfigurationManagerComplianceRequired { get; set; }
-    public bool TpmRequired { get; set; }
-    public string DeviceCompliancePolicyScript { get; set; }
     public List<string> ValidOperatingSystemBuildRanges { get; set; }
     [JsonProperty("assignments@odata.context")]
     public string AssignmentsOdataContext { get; set; }
