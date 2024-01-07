f3ac89ab7c5c966188568ab26846cd00279d2f28
srozemuller <s.rozemuller@exite.com>
2024-01-07 10:17:44 +0100

    created assigments model and added filter info to return human readable filternames

--- IntuneAssistant.Infrastructure/Interfaces/IAssignmentFiltersService.cs
+++ IntuneAssistant.Infrastructure/Interfaces/IAssignmentFiltersService.cs
@@ -7,7 +7,5 @@ public interface IAssignmentFiltersService
 {
     Task<List<DeviceAndAppManagementAssignmentFilter>?> GetAssignmentFiltersListAsync(string accessToken);
     Task<AssignmentFiltersDeviceEvaluationResponse> GetAssignmentFilterDeviceListAsync(string accessToken, string filterId);
-
     Task<DeviceAndAppManagementAssignmentFilter> GetAssignmentFilterInfoAsync(string accessToken, string filterId);
-
 }
