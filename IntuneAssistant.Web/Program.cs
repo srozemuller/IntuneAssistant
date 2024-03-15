c3cc2f4c6de003644463690050218d77b9f0a42d
srozemuller <s.rozemuller@exite.com>
2024-03-15 07:50:21 -0700

    updated group assignments page

--- IntuneAssistant.Web/Program.cs
+++ IntuneAssistant.Web/Program.cs
@@ -9,6 +9,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<ICompliancePoliciesService, CompliancePolicyService>();
 builder.Services.AddScoped<IConfigurationPolicyService, ConfigurationPolicyService>();
@@ -19,6 +20,7 @@ builder.Services.AddMsalAuthentication(options =>
 {
     builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
     options.ProviderOptions.DefaultAccessTokenScopes.Add("DeviceManagementApps.Read.All");
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("Group.Read.All");
 });
 
 await builder.Build().RunAsync();
