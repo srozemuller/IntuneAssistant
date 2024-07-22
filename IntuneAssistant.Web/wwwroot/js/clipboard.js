35d54c1b9e06bcd2bec785901cf00487f62a9a38
Sander Rozemuller <43162899+srozemuller@users.noreply.github.com>
2024-07-22 10:30:36 +0200

    added idpowertools integration

---
+++ IntuneAssistant.Web/wwwroot/js/clipboard.js
@@ -0,0 +1,7 @@
+function copyToClipboard(text) {
+    navigator.clipboard.writeText(text).then(function() {
+        console.log('Copying to clipboard was successful!');
+    }, function(err) {
+        console.error('Could not copy text: ', err);
+    });
+}
