a0c7aa787c7613f5a8443a6e0c02f1a8342c90c1
Sander Rozemuller <43162899+srozemuller@users.noreply.github.com>
2024-08-03 16:05:12 +0200

    Revert "added onboarding cards"
    This reverts commit 14c78294f5ac8eb6f6c108602f8e3379aa8f378a.

--- IntuneAssistant.WebInterface/src/components/auth/authHandler.js
+++ IntuneAssistant.WebInterface/src/components/auth/authHandler.js
@@ -1,4 +1,4 @@
-import authService from './msalservice.js';
+import authService from '../../components/auth/msalservice.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
     await authService.initialize(); // Ensure authService is initialized
--- IntuneAssistant.WebInterface/src/types/index.d.ts
+++ IntuneAssistant.WebInterface/src/types/index.d.ts
@@ -98,13 +98,3 @@ export type InfoLdg = {
   description: string;
   list: InfoList[];
 };
-
-export type Environment = {
-  environment: string;
-  displayName: string;
-  url: string;
-};
-
-export type BlueshiftM365ConsentCardProps = {
-  environments: Environment[];
-};
