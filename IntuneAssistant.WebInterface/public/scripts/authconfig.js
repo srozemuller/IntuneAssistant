7dd72dd5a08078e234ad032e3cb7be3f15eb05f6
Sander Rozemuller <43162899+srozemuller@users.noreply.github.com>
2024-09-11 22:16:46 +0200

    Update authconfig.js

--- IntuneAssistant.WebInterface/public/scripts/authconfig.js
+++ IntuneAssistant.WebInterface/public/scripts/authconfig.js
@@ -3,7 +3,7 @@ const msalConfig = {
     auth: {
         clientId: '131386a4-d462-4270-ac50-7ebc4685da14',
         authority: 'https://login.microsoftonline.com/organizations',
-        redirectUri: 'http://localhost:4321/authentication/login-callback',
+        redirectUri: 'https://intuneassistant.cloud',
     },
     cache: {
         cacheLocation: "sessionStorage",
