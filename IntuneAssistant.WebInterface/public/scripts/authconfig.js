bb8a3ecfb0503473e9d5645d593001861cf77901
Sander Rozemuller <43162899+srozemuller@users.noreply.github.com>
2024-11-08 14:19:40 +0100

    updated ids

--- IntuneAssistant.WebInterface/public/scripts/authconfig.js
+++ IntuneAssistant.WebInterface/public/scripts/authconfig.js
@@ -1,7 +1,7 @@
 import * as msal from '../../node_modules/@azure/msal-browser';
 const msalConfig = {
     auth: {
-        clientId: '131386a4-d462-4270-ac50-7ebc4685da14',
+        clientId: '0f0f930f-a5c7-4da2-a985-8464d1ff51d0',
         authority: 'https://login.microsoftonline.com/organizations',
         redirectUri: 'https://intuneassistant.cloud',
     },
@@ -15,5 +15,5 @@ const msalInstance = new msal.PublicClientApplication(msalConfig);
 
 export { msalInstance };
 export const loginRequest = {
-    scopes: ["api://b0533a36-0d90-4634-9f08-99a50b78b477/access_as_user"]
+    scopes: ["api://6317a049-4e55-464f-80a1-0896b8309fec/access_as_user"]
 };
