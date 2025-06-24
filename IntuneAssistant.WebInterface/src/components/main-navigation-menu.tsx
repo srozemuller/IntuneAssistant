9c7879bcbe9388cb7e888077e511d2a410861e72
Sander Rozemuller <43162899+srozemuller@users.noreply.github.com>
2025-06-24 10:05:58 +0200

    updated onboarding image

--- IntuneAssistant.WebInterface/src/components/main-navigation-menu.tsx
+++ IntuneAssistant.WebInterface/src/components/main-navigation-menu.tsx
@@ -169,7 +169,7 @@ export function MainNavigationMenu() {
                                             className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
                                             href="/onboarding"
                                         >
-                                            <img src='/images/launch.png' alt="Logo" />
+                                            <img src='/images/onboarding.png' alt="Logo" />
                                             <div className="mb-2 mt-4 text-lg font-medium">
                                                 Onboarding
                                             </div>
