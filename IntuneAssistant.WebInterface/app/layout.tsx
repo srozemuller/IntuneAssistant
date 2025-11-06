ca68efe87d22af364aa66ac70e3474dcf7d33d36
Sander Rozemuller <43162899+srozemuller@users.noreply.github.com>
2025-11-06 08:36:06 +0100

    Update layout.tsx

--- IntuneAssistant.WebInterface/app/layout.tsx
+++ IntuneAssistant.WebInterface/app/layout.tsx
@@ -17,13 +17,15 @@ export default function RootLayout({
     children: React.ReactNode;
 }) {
 
+    const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
 
     return (
         <html lang="en" suppressHydrationWarning>
         <head>
+            {GA_MEASUREMENT_ID && (
                 <>
                     <Script
-                        src={`https://www.googletagmanager.com/gtag/js?id=G-1KSX6MPF8W`}
+                        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
                         strategy="afterInteractive"
                     />
                     <Script id="google-analytics" strategy="afterInteractive">
@@ -31,10 +33,11 @@ export default function RootLayout({
                             window.dataLayer = window.dataLayer || [];
                             function gtag(){dataLayer.push(arguments);}
                             gtag('js', new Date());
-                            gtag('config', 'G-1KSX6MPF8W');
+                            gtag('config', '${GA_MEASUREMENT_ID}');
                         `}
                     </Script>
                 </>
+            )}
         </head>
         <body className={inter.className}>
         <ClientLayout>
