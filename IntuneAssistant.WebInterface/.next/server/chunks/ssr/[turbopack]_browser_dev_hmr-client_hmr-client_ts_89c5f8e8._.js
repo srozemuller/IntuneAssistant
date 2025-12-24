ff10f4f12aed941464b9b6364999e09a2c3c1d5c
Sander Rozemuller <43162899+srozemuller@users.noreply.github.com>
2025-12-24 16:04:55 +0100

    new pages

---
+++ IntuneAssistant.WebInterface/.next/server/chunks/ssr/[turbopack]_browser_dev_hmr-client_hmr-client_ts_89c5f8e8._.js
@@ -0,0 +1,12 @@
+module.exports = [
+"[turbopack]/browser/dev/hmr-client/hmr-client.ts [app-ssr] (ecmascript, async loader)", ((__turbopack_context__) => {
+
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  "server/chunks/ssr/[turbopack]_browser_dev_hmr-client_hmr-client_ts_818f0fdf._.js"
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
+        return parentImport("[turbopack]/browser/dev/hmr-client/hmr-client.ts [app-ssr] (ecmascript)");
+    });
+});
+}),
+];
---
+++ IntuneAssistant.WebInterface/.next/static/chunks/app_layout_tsx_0a548d63._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__36b083e1._.css",
+  "static/chunks/_4e4cc0b9._.js",
+  "static/chunks/node_modules_@azure_msal-common_dist_9b03f66b._.js",
+  "static/chunks/node_modules_@azure_msal-browser_dist_d3ab8b54._.js",
+  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_56b6fd65._.js",
+  "static/chunks/node_modules_1593e1f0._.js"
+],
+    source: "dynamic"
+});
