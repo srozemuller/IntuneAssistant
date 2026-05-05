f353ec2aa223733dd704daf4285d10fe4637c5fd
Sander Rozemuller <43162899+srozemuller@users.noreply.github.com>
2026-05-05 08:58:48 +0200

    Removed unsupported jobs in worker

--- IntuneAssistant.WebInterface/.next/static/chunks/app_monitor_page_tsx_9ad2371d._.js
+++ IntuneAssistant.WebInterface/.next/static/chunks/app_monitor_page_tsx_9ad2371d._.js
@@ -1,7 +1,7 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/_47f9a029._.js"
+  "static/chunks/_66098bc0._.js"
 ],
     source: "dynamic"
 });
